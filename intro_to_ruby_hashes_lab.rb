e1065c891e1d2990e83dbbe79745dad61edeccbd
Toni  Diep <adiep123@gmail.com>
2020-05-06 18:10:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,8 +12,6 @@ end
 
 def monopoly_with_second_tier
   monopoly = {:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {}}
-  # monopoly = {:railroads => {:names => {}}}
-  # monopoly = {:railroads => {:rent_in_dollars => {}}}
 end
 
 def monopoly_with_third_tier
