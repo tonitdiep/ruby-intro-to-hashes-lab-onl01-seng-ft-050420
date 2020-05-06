58289135c8926bb9360c636a7deb42416a57f5f3
Toni  Diep <adiep123@gmail.com>
2020-05-06 18:00:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,6 +13,7 @@ end
 def monopoly_with_second_tier
   monopoly = {:railroads => {:pieces => 4}}
   monopoly = {:railroads => {:names => {}}}
+  monopoly = {:railroads => {:rent_in_dollars => {}}}
 end
 
 def monopoly_with_third_tier
